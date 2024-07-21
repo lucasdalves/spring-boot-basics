15111353f017576b56bde2ecfd2e9f39628bef0a
Lucas Alves <lucas.davi.alves@hotmail.com>
2024-07-21 20:02:27 -0300

    feat hr payroll wip

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
@@ -0,0 +1,45 @@
+package com.devsuperior.hrpayroll.entities;
+
+import java.io.Serializable;
+
+public class Worker implements Serializable {
+    private Long id;
+    private String name;
+    private double DailyIncome;
+
+    public Worker(Long id, String name, double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        DailyIncome = dailyIncome;
+    }
+
+
+    public Worker() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDailyIncome() {
+        return DailyIncome;
+    }
+
+    public void setDailyIncome(double dailyIncome) {
+        DailyIncome = dailyIncome;
+    }
+
+
+}
